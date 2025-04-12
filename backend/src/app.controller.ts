c94bd18d8493ef2f9f6f7dc0ae3284abc5e7c21d
Kaylan Gokal <2724001@students.wits.ac.za>
2025-04-12 20:23:30 +0200

    Initial set up of branch and auth0 implementation

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('protected')
+  @UseGuards(JwtAuthGuard)
+  getProtected() {
+    return { message: 'This is a protected route!' };
   }
 }
