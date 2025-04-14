b9f6ee7206bdb84739b223e946f3eb8cc3ddca24
Jaishil <2602867@students.wits.ac.za>
2025-04-14 13:40:52 +0200

    Final

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
 import { Auth0Provider } from '@auth0/auth0-react';
+import { Routes, Route } from 'react-router-dom';
+import { CartProvider } from './context/ContextCart';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -7,6 +9,7 @@ import SearchBar from './components/SearchBar';
 import FeaturedProducts from './components/FeaturedProducts';
 import WhyChooseUs from './components/WhyChooseUs';
 import Footer from './components/Footer';
+import CartPage from './pages/CartPage';
 
 function App() {
   const handleSearch = (query: string) => {
@@ -22,20 +25,29 @@ function App() {
         redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
       }}
     >
-      <div className="app">
-        <Navbar />
-        <main>
-          <Hero />
-          <ExploreShops />
-          <SearchBar onSearch={handleSearch} />
-          <FeaturedProducts />
-          <WhyChooseUs />
-        </main>
-        <Footer />
-      </div>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Hero />
+                  <ExploreShops />
+                  <SearchBar onSearch={handleSearch} />
+                  <FeaturedProducts />
+                  <WhyChooseUs />
+                </>
+              } />
+              <Route path="/cart" element={<CartPage />} />
+
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </CartProvider>
     </Auth0Provider>
   );
 }
 
 export default App;
-
