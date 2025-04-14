c6923c21691fafc02b67545a167565ae3e190940
MuzzammilWits <2731823@students.wits.ac.za>
2025-04-14 11:24:37 +0200

    testing deploy

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { AppService } from './app.service';
+
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {} // ✅ Inject service here
+
   @Get('protected')
   @UseGuards(JwtAuthGuard)
   getProtected() {
     return { message: 'This is a protected route!' };
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello(); // ✅ Now TypeScript is happy
   }
 }
