cf01c9db7f6ad41d184a640cdad2b5c261992540
MuzzammilWits <2731823@students.wits.ac.za>
2025-04-15 21:36:11 +0200

    i am trying

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,33 +1,24 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-//import { UserModule } from './user/user.module';
+// import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        type: 'mssql',
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get('DB_PORT', '1433'), 10),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        synchronize: false,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        extra: {
-          // encrypt: true,
-        },
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'hoodgoodsserver.database.windows.net',
+      port: 1433,
+      username: 'Muzzammil',
+      password: 'Password123!',
+      database: 'HoodGoodsDB',
+      synchronize: false,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      extra: {
+        // encrypt: true,
+      },
     }),
 
     AuthModule,
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
-import { ConfigModule } from '@nestjs/config';
-
 
 @Module({
-  imports: [ConfigModule.forRoot(),PassportModule.register({ defaultStrategy: 'jwt' })],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [JwtStrategy],
   exports: [PassportModule],
 })
