4c4d244aa9ea55c08ac456f7cc57dff9004e1e20
MuzzammilWits <2731823@students.wits.ac.za>
2025-04-15 21:36:11 +0200

    i am trying

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import * as jwksRsa from 'jwks-rsa';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private configService: ConfigService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      audience: configService.get(`AUTH0_AUDIENCE`) ,
-      issuer: `https://${configService.get('AUTH0_DOMAIN')}/`,
+      audience: 'https://hoodgoods-api.com',
+      issuer: 'https://dev-vp08yyf1exxzeenl.us.auth0.com/',
       algorithms: ['RS256'],
       secretOrKeyProvider: jwksRsa.passportJwtSecret({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        // jwksUri: `https://dev-vp08yyf1exxzeenl.us.auth0.com/.well-known/jwks.json`,
-        jwksUri: `https://${configService.get('AUTH0_DOMAIN')}/.well-known/jwks.json`,
+        jwksUri: 'https://dev-vp08yyf1exxzeenl.us.auth0.com/.well-known/jwks.json',
       }),
     });
   }
