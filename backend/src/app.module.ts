8bd805f5cf68511f2e5c8bff01a1418cd7daa770
MuzzammilWits <2731823@students.wits.ac.za>
2025-04-15 17:07:56 +0200

    Update app.module.ts for DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: 'mssql',
+        host: config.get('DB_HOST'),
+        port: parseInt(config.get('DB_PORT', '1433'), 10),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        synchronize: false,
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        extra: {
+          // encrypt: true,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
