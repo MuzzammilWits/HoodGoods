e69b135077e84f23f503a3417bbac1ee431e4b26
MuzzammilWits <2731823@students.wits.ac.za>
2025-04-17 12:14:46 +0200

    updated this stuff to work with the new dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 // src/app.module.ts
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,26 +7,18 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        type: 'mssql',
-        host: config.get('DB_HOST'),
-        port: parseInt(config.get('DB_PORT', '1433'), 10),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_NAME'),
-        synchronize: false,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        extra: {
-          //encrypt: true,
-        },
-      }),
-      inject: [ConfigService],
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'hoodgoodsserver.database.windows.net',
+      port: 1433,
+      username: 'Muzzammil',
+      password: 'Password123!',
+      database: 'HoodGoodsDB',
+      synchronize: false,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      extra: {
+        // encrypt: true,
+      },
     }),
 
     UserModule,
