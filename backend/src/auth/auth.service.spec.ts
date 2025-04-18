e3abb0d855f9febc5f87d81d29c1aee7f472c9c9
MuzzammilWits <2731823@students.wits.ac.za>
2025-04-18 15:32:37 +0200

    Update auth.service.spec.ts
    Made the tests work

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,15 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+
+// Create a mock repository
+const mockUserRepository = () => ({
+  findOne: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
 
 describe('AuthService', () => {
   let service: AuthService;
+  let userRepository: jest.Mocked<Partial<Repository<User>>>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    userRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
