f8c6de21b923a9800d7e1753ecab2505e318d43c
MuzzammilWits <2731823@students.wits.ac.za>
2025-04-22 00:35:55 +0200

    lil fix on tests

--- frontend/src/components/Navbar.test.tsx
+++ frontend/src/components/Navbar.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor, act } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom'; // Needed for <Link> components
 import { vi } from 'vitest'; // Or import { jest } from '@jest/globals'; if using Jest
