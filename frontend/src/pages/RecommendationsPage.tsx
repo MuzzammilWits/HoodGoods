919518bb3e8c2e0af486d418593889e0e8c16c4e
Shervaan Govinder <shivesar11@gmail.com>
2025-05-25 16:49:08 +0200

    U.I changes

--- frontend/src/pages/RecommendationsPage.tsx
+++ frontend/src/pages/RecommendationsPage.tsx
@@ -1,14 +1,21 @@
 // frontend/src/pages/RecommendationsPage.tsx
 import React from 'react';
 import BestSellersList from '../components/recommendations/BestSellersList';
+import { useNavigate } from 'react-router-dom';
 import './RecommendationsPage.css'; // Create this CSS file
 
 const RecommendationsPage: React.FC = () => {
+  const navigate = useNavigate();
   return (
     // Changed div to main for the primary content container of the page
     <main className="recommendations-page-container" style={{ paddingTop: '20px', paddingBottom: '20px' }}>
         <h1 className="main-titles">Discover Products You Might Like</h1>
         <p className="main-titles-sub">Based on current trends and popular items.</p>
+        <section className="back-button-container">
+        <button onClick={() => navigate('/products')} className="back-button">
+          Back to Products
+        </button>
+        </section>
 
 
       <section className="recommendation-section">
